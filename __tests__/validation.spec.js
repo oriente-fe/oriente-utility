90e4e4bea4d4f8062c1aa60e9f6e787431d5c669
Tony Pai <tony77794@gmail.com>
2020-01-02 18:21:59 +0800

    Add string length validation

---
+++ __tests__/validation.spec.js
@@ -0,0 +1,15 @@
+import { lengthEqualTo } from '~/validation'
+
+describe('lengthEqualTo', () => {
+  const err = 'Input length should be 4'
+  it('should return true when equal', () => {
+    expect(lengthEqualTo(4, err)('1234')).toBe(true)
+  })
+  it('should return error message when not equal', () => {
+    expect(lengthEqualTo(4, err)('123')).toBe(err)
+  })
+  it('should auto-correct the type of input value', () => {
+    expect(lengthEqualTo(4, err)(1234)).toBe(true)
+    expect(lengthEqualTo(4, err)(123)).toBe(err)
+  })
+})
---
+++ src/validation.js
@@ -0,0 +1,3 @@
+export const lengthEqualTo = (n = 0, err = '') => value => {
+  return String(value).length === n || err
+}
